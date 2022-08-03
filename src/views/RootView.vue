24579436225d828066a7645f1ae028c57776dfc4
Vitaliy Tamashevich <vtamashevich@exadel.com>
2022-08-03 16:13:30 +0200

    init project

---
+++ src/views/RootView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="root">
+    <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
+    <HelloWorld msg="sign in or sign up" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "RootView",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
